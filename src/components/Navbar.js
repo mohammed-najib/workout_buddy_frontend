876231616966465d924c667c1b0abd265ca9a977
mohammed-najib <mohammed.njeeb@gmail.com>
2022-08-02 13:12:17 +0300

    made user auth presistent through web reloads

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -15,11 +17,17 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
-          <button onClick={handleClick}>Logout</button>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
+          {user != null ? (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
+            </div>
+          ) : (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
